fb2a470c346e00f02d5c67cdb0627393aecea147
andrii.boltianskyi <andrii.boltianskyi@eleks.com>
2020-11-06 17:05:45 +0200

    234

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {log} from '../assets/log';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'log-reader';
+  result: Map<string, number>;
+  total = 0;
+  asd: string = 'sdjkfndsffg';
+
+  constructor() {
+    this.parse();
+  }
+
+  parse(): void {
+    this.asd = 1233;
+    const splitLog = log.split('\n');
+    this.result = new Map();
+    let splitLine;
+
+    splitLog.forEach(line => {
+      splitLine = line.split(':');
+      if (splitLine.length > 1) {
+        const current = parseInt(splitLine[1], 10);
+        this.result.set(splitLine[0], (this.result.get(splitLine[0]) | 0) + current);
+        this.total += current;
+      }
+    });
+    console.log(this.result);
+  }
 }
